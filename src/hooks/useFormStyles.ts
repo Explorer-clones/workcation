a350f1d60f060d40940c7882562928ae62d28e6b
user <progexplorer@gmail.com>
2020-04-03 08:52:49 +0200

    refactor(hooks/useformstyles): extract common styling into separate partials

---
+++ src/hooks/useFormStyles.ts
@@ -0,0 +1,16 @@
+import { css, SerializedStyles } from "@emotion/core"
+import tw from "tailwind.macro"
+
+export const useBasicFormStyling = (): SerializedStyles => {
+  return css`
+    ${tw`px-3 py-2 text-base leading-normal bg-white border border-gray-300 rounded appearance-none focus:outline-none focus:shadow-outline`}
+  `
+}
+
+export const usePlaceholderFormDefaults = (): SerializedStyles => {
+  return css`
+    &::placeholder {
+      ${tw`text-base text-gray-500 opacity-100`}
+    }
+  `
+}

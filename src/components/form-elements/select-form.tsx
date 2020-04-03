e03cfdeb8d0f7c3bff98edcbe637e5a9260b95e0
user <progexplorer@gmail.com>
2020-04-03 08:51:13 +0200

    feat(form-elements/select-form): add select component

---
+++ src/components/form-elements/select-form.tsx
@@ -0,0 +1,59 @@
+import React, { ReactNode } from "react"
+import { SerializedStyles } from "@emotion/core"
+import svgToMiniDataURI from "mini-svg-data-uri"
+import { useBasicFormStyling } from "../../hooks/useFormStyles"
+import { css } from "@emotion/core"
+import { useTheme } from "emotion-theming"
+import tw from "tailwind.macro"
+import { Theme } from "../../utils/tailwind-types"
+type SelectFormProps = {
+  css?: SerializedStyles
+  name: string
+  id: string
+  children: ReactNode
+}
+
+export const SelectForm: React.FC<SelectFormProps> = ({
+  id,
+  name,
+  children,
+  ...props
+}) => {
+  const theme = useTheme<Theme>()
+
+  type svgToDataURI = (value: string) => string
+
+  const applyColorsToSVGIcon = (
+    color: string,
+    svgToMiniDataURI: svgToDataURI
+  ): string =>
+    `"${svgToMiniDataURI(
+      `<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='${color}'>
+        <path
+          className='heroicon-ui'
+          d='M15.3 9.3a1 1 0 0 1 1.4 1.4l-4 4a1 1 0 0 1-1.4 0l-4-4a1 1 0 0 1 1.4-1.4l3.3 3.29 3.3-3.3z'
+        />
+      </svg>`
+    )}"`
+
+  return (
+    <select
+      name={name}
+      id={id}
+      {...props}
+      css={css`
+        ${useBasicFormStyling()}
+        ${tw`pr-10`};
+        background-position: ${`right ${theme.spacing[2]} center`};
+        background-size: 1.5em 1.5em;
+        background-repeat: no-repeat;
+        background-image: url(${applyColorsToSVGIcon(
+          theme.colors.gray[500],
+          svgToMiniDataURI
+        )});
+      `}
+    >
+      {children}
+    </select>
+  )
+}
